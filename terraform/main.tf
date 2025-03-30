3c541a0bd8edf9cffc90f7af31f69cbd7d56bafb
AdminMod18 <hugoa.latorrep@konradlorenz.edu.co>
2025-03-30 18:00:30 -0500

    Ajuste correccion de permisos terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
 provider "kubernetes" {
-  host                   = "https://127.0.0.1:64671"
-  client_certificate     = file("~/.minikube/profiles/minikube/client.crt")
-  client_key             = file("~/.minikube/profiles/minikube/client.key")
-  cluster_ca_certificate = file("~/.minikube/ca.crt")
-}
-
-resource "kubernetes_namespace" "mi_proyecto" {
-  metadata {
-    name = "mi-proyecto"
-  }
+  config_path = "~/.kube/config"
 }
